a1e0e098d7b915b9a76e905efb8a7a2130fbe450
Joao Gomes <joaopfg.2002@gmail.com>
2020-05-13 21:12:08 +0100

    done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 
@@ -24,12 +25,67 @@ app.post("/" , function (req,res) {
     const lastName = req.body.LastName;
     const email = req.body.email;
 
-    console.log(firstName + " - "  +  lastName + " - " + email);
+    var data = {
+            
+        members : [
+            {
+                email_address: email,
+                status : "subscribed",
+                merge_fields : {
+                    FNAME : firstName,
+                    LNAME : lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us18.api.mailchimp.com/3.0/lists/f6294206ef";
+
+    const options = {
+
+        method : "POST",
+        auth: "JoaoGomes5:ed1824ff58850ee79892bd12478218e8-us18"
+
+    };
+
+   const request =  https.request(url,options, function (response) { 
+
+        if (response.statusCode === 200){
+
+            res.sendFile(__dirname + "/success.html");
+
+        }else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data" , function (data) { 
+
+            console.log(JSON.parse(data));
+            
+    });
+
+
+    });
+
+    request.write(jsonData);
+    request.end();
     
 
+
+});
+
+app.post("/success" , function (req, res) {
+
+    res.redirect("/");
     
+});
 
+app.post("/failure" , function (req, res) {
 
+    res.redirect("/");
+    
 });
 
 
@@ -42,3 +98,8 @@ app.listen(port, function() {
     console.log("Server on port: " + port);
     
 });
+
+//API KEY
+//ed1824ff58850ee79892bd12478218e8-us18
+//LIST ID
+//f6294206ef
